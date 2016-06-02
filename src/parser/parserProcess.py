213abd25399196c536a15f795fae58efeb03ce47
cj <163ever@163.com>
2016-06-02 15:22:21 +0800

    下载器代码

--- src/parser/parserProcess.py
+++ src/parser/parserProcess.py
@@ -69,117 +69,125 @@ ruleNameUrl = urlRule['rule_name']
 listCmdUrl = urlRule['list']
 urlCmd = urlRule['data']['url']
 
-counter = 0
-while counter < 50:
-    # 取得download文件列表
-    listFile = os.listdir(downloadPath)
-    it = iter(listFile)
+try:
+    counter = 0
+    while counter < 50:
+        # 取得download文件列表
+        listFile = os.listdir(downloadPath)
+        it = iter(listFile)
 
-    # 遍历download文件
-    for fileName in it:
-        # url是否抓取标志
-        urlSeizeFlg = 1
+        # 遍历download文件
+        for fileName in it:
+            # url是否抓取标志
+            urlSeizeFlg = 1
 
-        filePath = downloadPath + os.sep + fileName
-        file = open(filePath, "r+", encoding='utf-8')
+            filePath = downloadPath + os.sep + fileName
+            file = open(filePath, "r+", encoding='utf-8')
 
-        # 得到url
-        url = file.readline()
-        if url == "":
-            continue
-        if url.replace("\n", "").endswith('100/'):
-            urlSeizeFlg = 0
-        # 得到下载地址的Md5值
-        fileNameMd5 = get_md5(url)
+            # 得到url
+            url = file.readline()
+            if url == "":
+                continue
+            if url.replace("\n", "").endswith('100/'):
+                urlSeizeFlg = 0
+            # 得到下载地址的Md5值
+            fileNameMd5 = get_md5(url)
 
-        parserResultFilePath = parserResultPath + os.sep + fileNameMd5 + ".json"
-        # 检查是否解析过
-        if os.path.exists(parserResultFilePath):
-            continue
-        else:
-            # 结果文件不存在则新建，并保存结果数据
-            html = file.read()
-            soup = BeautifulSoup(html, "html.parser")
-
-            '''抓取数据url地址'''
-            if urlSeizeFlg == 1:
-                urlList = eval(listCmdUrl)
-                nextPageUrl = prefixUrl + eval(urlCmd)
-
-            '''抓取楼盘数据'''
-            houseList = eval(listCmd)
-            # 定义解析结果集合
-            records = []
-            for item in houseList:
-                hid = eval(idCmd)
-                ''' 得到抓取的楼盘信息 '''
-                estateName = eval(estateNameCmd)
-                estateAddress = eval(estateAddressCmd)
-
-                # 得到子元素的数量
-                elCount = get_element_count(item)
-
-                roomType = eval(roomTypeCmd) if elCount > 0 else ''
-                floor = eval(floorCmd) if elCount > 2 else ''
-                orientation = eval(orientationCmd) if elCount > 4 else ''
-                buildingAge = eval(buildingAgeCmd) if elCount > 6 else ''
-
-                area = eval(areaCmd)
-                totalPrice = eval(totalPriceCmd) + "万"
-                unitPrice = eval(unitPriceCmd)
-
-                # print(hid + '->' + estate_name, roomType, floor, orientation, buildingAge, sep=',')
-
-                # 抓取数据封装
-                record = dict(data_name='houseInfo')
-                record["id"] = hid
-
-                items = dict()
-                items["estate_name"] = estateName
-                items["estate_address"] = estateAddress
-                items["room_type"] = roomType
-                items["floor"] = floor
-                items["orientation"] = orientation
-                items["building_age"] = buildingAge
-                items["area"] = area
-                items["total_price"] = totalPrice
-                items["unit_price"] = unitPrice
-                record["items"] = items
-                records.append(record)
-
-        # 文件读取(test)
-        # parserResultFilePath = parserResultPath + "\\" + fileNameMd5 + ".json"
-        # with open(parserResultFilePath, "r") as file1:
-        #     msg = json.load(file1)
-        # for item in msg:
-        #     print(item["data_name"], item["id"])
-        #     print(item["items"]["estate_name"], item["items"]["room_type"], item["items"]["building_age"])
-
-        # url地址保存
-        if urlSeizeFlg == 1:
-            FORMAT_DATE = '%Y%m%d%H%M%S'
-            urlFilePath = urlPath + os.sep + time.strftime(FORMAT_DATE) + ".txt"
-            if os.path.exists(urlFilePath):
-                pass
-                # file = open(urlFilePath, 'a', encoding='utf-8')
+            parserResultFilePath = parserResultPath + os.sep + fileNameMd5 + ".json"
+            # 检查是否解析过
+            if os.path.exists(parserResultFilePath):
+                continue
             else:
-                fileUrl = open(urlFilePath, 'w', encoding='utf-8')
-                fileUrl.write(nextPageUrl)
-                fileUrl.write('\n')
-                fileUrl.close()
+                # 结果文件不存在则新建，并保存结果数据
+                html = file.read()
+                soup = BeautifulSoup(html, "html.parser")
 
-        # 抓取数据保存
-        encodeD = json.dumps(records, ensure_ascii=False)
-        with open(parserResultFilePath, 'w', encoding='utf-8') as f:
-            f.write(encodeD)
+                '''抓取数据url地址'''
+                if urlSeizeFlg == 1:
+                    try:
+                        urlList = eval(listCmdUrl)
+                        nextPageUrl = prefixUrl + eval(urlCmd)
+                    except Exception as err:
+                        print("error: {0}".format(err))
+                        exit()
 
-        # 重置
-        counter = -1
+                '''抓取楼盘数据'''
+                houseList = eval(listCmd)
+                # 定义解析结果集合
+                records = []
+                for item in houseList:
+                    hid = eval(idCmd)
+                    ''' 得到抓取的楼盘信息 '''
+                    estateName = eval(estateNameCmd)
+                    estateAddress = eval(estateAddressCmd)
 
-    file.seek(0, 0)
-    file.close()
+                    # 得到子元素的数量
+                    elCount = get_element_count(item)
 
-    if counter > -1:
-        time.sleep(2)
-    counter += 1
+                    roomType = eval(roomTypeCmd) if elCount > 0 else ''
+                    floor = eval(floorCmd) if elCount > 2 else ''
+                    orientation = eval(orientationCmd) if elCount > 4 else ''
+                    buildingAge = eval(buildingAgeCmd) if elCount > 6 else ''
+
+                    area = eval(areaCmd)
+                    totalPrice = eval(totalPriceCmd) + "万"
+                    unitPrice = eval(unitPriceCmd)
+
+                    # print(hid + '->' + estate_name, roomType, floor, orientation, buildingAge, sep=',')
+
+                    # 抓取数据封装
+                    record = dict(data_name='houseInfo')
+                    record["id"] = hid
+
+                    items = dict()
+                    items["estate_name"] = estateName
+                    items["estate_address"] = estateAddress
+                    items["room_type"] = roomType
+                    items["floor"] = floor
+                    items["orientation"] = orientation
+                    items["building_age"] = buildingAge
+                    items["area"] = area
+                    items["total_price"] = totalPrice
+                    items["unit_price"] = unitPrice
+                    record["items"] = items
+                    records.append(record)
+
+            # 文件读取(test)
+            # parserResultFilePath = parserResultPath + "\\" + fileNameMd5 + ".json"
+            # with open(parserResultFilePath, "r") as file1:
+            #     msg = json.load(file1)
+            # for item in msg:
+            #     print(item["data_name"], item["id"])
+            #     print(item["items"]["estate_name"], item["items"]["room_type"], item["items"]["building_age"])
+
+            # url地址保存
+            if urlSeizeFlg == 1:
+                FORMAT_DATE = '%Y%m%d%H%M%S'
+                urlFilePath = urlPath + os.sep + time.strftime(FORMAT_DATE) + ".txt"
+                if os.path.exists(urlFilePath):
+                    pass
+                    # file = open(urlFilePath, 'a', encoding='utf-8')
+                else:
+                    fileUrl = open(urlFilePath, 'w', encoding='utf-8')
+                    fileUrl.write(nextPageUrl)
+                    fileUrl.write('\n')
+                    fileUrl.close()
+
+            # 抓取数据保存
+            encodeD = json.dumps(records, ensure_ascii=False)
+            with open(parserResultFilePath, 'w', encoding='utf-8') as f:
+                f.write(encodeD)
+
+            # 重置
+            counter = -1
+
+        file.seek(0, 0)
+        file.close()
+
+        if counter > -1:
+            time.sleep(2)
+        counter += 1
+except Exception as err:
+    print("error: {0}".format(err))
+
 print("----------------------------【parser finished】----------------------------------")
