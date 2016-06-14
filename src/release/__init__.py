89995b6c0f3fc7c3285e67a8b8ae5495831fef4c
cj <163ever@163.com>
2016-06-14 10:38:31 +0800

    下载器代码

--- src/release/__init__.py
+++ src/release/__init__.py
@@ -10,6 +10,7 @@ os.system('python dataInsertProcess.py {0} {1}'.format(argv1, argv2))
 # argv1 = 'D:/task'
 # argv2 = 'http://esf.sh.fang.com/house/i381'
 # argv3 = 'D:\\PycharmProjects\\NDCS\\src\\rule'
+# argv4 = 30
 
-# os.system('python controlProcess.py {0} {1} {2}'.format(argv1, argv2, argv3))
+# os.system('python controlProcess.py {0} {1} {2}'.format(argv1, argv2, argv3, argv4))
 
--- src/release/controlProcess.py
+++ src/release/controlProcess.py
@@ -15,10 +15,12 @@ def getplatform():
 rootPath = sys.argv[1]
 startUrl = sys.argv[2]
 rulePath = sys.argv[3]
+waitMinutes = sys.argv[4]
 
 # 创建任务文件目录
 FORMAT_DATE = '%Y%m%d%H%M%S'
-taskPath = rootPath + os.sep + time.strftime(FORMAT_DATE)
+parseTime = time.strftime(FORMAT_DATE)
+taskPath = rootPath + os.sep + parseTime
 if not os.path.exists(taskPath):
     os.mkdir(taskPath)
 
@@ -29,6 +31,10 @@ if getplatform() == 'Windows':
 else:
     os.system('python DownLoadProcess.py {0} {1} &'.format(taskPath, startUrl))
     os.system('python parserProcess.py {0} {1} &'.format(taskPath, rulePath))
+
+# 等待一段时间后做数据插入
+time.sleep(waitMinutes * 60)
+os.system('python dataInsertProcess.py {0} {1}'.format(rootPath, parseTime))
 
 
 
